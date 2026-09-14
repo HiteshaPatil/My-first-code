b7ad474492394a25f93f06dc597bdbe1d6bc4720
HiteshaPatil <patilhitesha18308@gmail.com>
2026-09-14 18:58:53 +0530

    Create Add.c

---
+++ Add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  printf("Enter value of a:");
+  scanf("%d",&a);
+  printf("Enter value of b:");
+  scanf("%d",&b);
+  add=a+b;
+  printf("add=%d\n",add);
+  return 0;
+}
+
+//output
+//Enter value of a:10
+//Enter value of b:20
+//sum=30
