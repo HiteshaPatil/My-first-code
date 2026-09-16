eef9c24ffa7c2bff27b47245b3135c03dbe386b3
HiteshaPatil <patilhitesha18308@gmail.com>
2026-09-16 21:38:05 +0530

    Correct area calculation in triangle area program
    Fixed area calculation for right-angled triangle.

---
+++ Right angled Triangle area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int area,base,height;
+  printf("enter base and height:");
+  scanf("%d %d",&base,&height);
+  area= 1/2*base*height;
+  printf("area=%d",area);
+  return 0;
+}
