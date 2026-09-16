5aaed132c863188224ec5ab5fd6143211e10e6a4
HiteshaPatil <patilhitesha18308@gmail.com>
2026-09-16 21:19:51 +0530

    Implement area and perimeter calculations for square
    Fix variable usage and add area and perimeter calculations.

---
+++ area and perimeter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int square,side;
+  printf("Enter side of square:");
+  scanf("%d",side);
+  area=side*side;
+  perimeter=4*side;
+  printf("area=%d",area);
+  printf("square=%d",square);
+  return 0;
+}
